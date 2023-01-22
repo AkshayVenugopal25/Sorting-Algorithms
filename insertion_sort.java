ba8d28fb4770fe16902b694d7b52d3ee0cb992ff
AkshayVenugopal25 <akshay.venu25@gmail.com>
2023-01-22 20:58:50 +0530

    insertion sort with java

---
+++ insertion_sort.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+import static java.lang.Math.abs;
+
+public class insertion_sort
+{
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        int arrayLength = 10;
+        int[] arr = new int[arrayLength];
+
+        for(int i = 0; i < arrayLength; ++i)
+        {
+            arr[i] = abs(random.nextInt() % 10);
+        }
+
+        System.out.println("Array before sorting : ");
+        printArray(arr);
+        bubbleSort(arr);
+        System.out.println("Array after sorting : ");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr)
+    {
+        for(int i = 0; i < arr.length; ++i)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+        int arrayLength = arr.length;
+
+        for(int i = 1; i < arrayLength - 1; ++i)
+        {
+            int j = i;
+
+            while(j > 0 && arr[j - 1] > arr[j])
+            {
+                // swapping
+
+                int tempElement = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = tempElement;
+
+                j--;
+            }
+        }
+    }
+}
